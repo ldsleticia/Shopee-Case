877dcaa3d2c977d0240f122093b50bedf8b541e9
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2021-12-10 12:07:02 -0300

    salvando alterações

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -35,7 +35,7 @@ public class Main {
             System.out.println("*----------------!! COMPRE DE TUDO !!----------------*");
             System.out.println("*************************************************************");
             System.out.println("1 - Mostrar Vendedores");
-            System.out.println("2 - Mostrar Produtos");
+            System.out.println("2 - Comprar");
             System.out.println("3 - Sacola");
             System.out.println("4 - Confirmar compra");
             System.out.println("*************************************************************");
@@ -51,11 +51,12 @@ public class Main {
                 case 1:
                     Utils.limparTela();
                     //mostrarvendedores();
+                    System.out.println("Digite o nome do vendedor que você deseja");
                     Utils.limparTela();
                     break;
                 case 2:
                     Utils.limparTela();
-                    //mostrarprodutodovendedorsolicitado();
+                    System.out.println("Digite o código do produto que você quer comprar");
                     Utils.limparTela();
                     break;
                 case 3:
@@ -75,4 +76,38 @@ public class Main {
 
         }while (opcao != 0);
     }
+
+
+    //    public static void mostrarvendedores(){
+//        String vendedor = new String();
+//        if(vendedor == "antunes" || vendedor == "Antunes" || vendedor == "ANTUNES"){
+//            lojaantunes.setNomeDoProduto("Batom");
+//            lojaantunes.setIdProduto(1);
+//            lojaantunes.setPrecodoProduro(3.99);
+//            lojaantunes.setEstoque(3);
+//        } else if(vendedor == "santos" || vendedor == "Santos" || vendedor == "SANTOS"){
+//            lojasantos.setNomeDoProduto("Anel Cinderela");
+//            lojasantos.setIdProduto(2);
+//            lojasantos.setPrecodoProduro(10.00);
+//            lojasantos.setEstoque(6);
+//        } else if(vendedor == "ferreira" || vendedor == "Ferreira" || vendedor == "FERREIRA"){
+//            lojaferreira.setNomeDoProduto("Chave de Fenda");
+//            lojaferreira.setIdProduto(3);
+//            lojaferreira.setPrecodoProduro(5.36);
+//            lojaferreira.setEstoque(9);
+//        } else if(vendedor == "salgueiro" || vendedor == "Salgueiro" || vendedor == "SALGUEIRO"){
+//            lojasalgueiro.setNomeDoProduto("Coxinha Congelada");
+//            lojasalgueiro.setIdProduto(4);
+//            lojasalgueiro.setPrecodoProduro(5.99);
+//            lojasalgueiro.setEstoque(30);
+//        } else if(vendedor == "figueira" || vendedor == "Figueira" || vendedor == "FIGUEIRA"){
+//            lojasalgueiro.setNomeDoProduto("Coxinha Congelada");
+//            lojasalgueiro.setIdProduto(4);
+//            lojasalgueiro.setPrecodoProduro(5.99);
+//            lojasalgueiro.setEstoque(30);
+//        } else{
+//            System.out.println("Digite um vendedor válido");
+//        }
+//    }
+
 }
