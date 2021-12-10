bf393262f6f9d71149d9be83fff5c1ef8e8a1e50
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2021-12-10 11:27:05 -0300

    salvando alterações

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -2,7 +2,7 @@ package utils;
 
 public class Utils {
     public static void limparTela(){
-        for (int i = 0; i < 13; i++){
+        for (int i = 0; i < 2; i++){
             System.out.println(" ");
         }
     }
